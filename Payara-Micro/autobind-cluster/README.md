f713d77e1241408d3a3b9a3b82bb46746fdd78e2
Steve Millidge <steve@payara.co>
2015-08-18 19:58:32 +0100

    Added Payara Micro Autobind example

---
+++ Payara-Micro/autobind-cluster/README.md
@@ -0,0 +1,12 @@
+# Autobind Cluster
+
+This Payara Micro example shows you how to bootstrap a Payara Micro Server so that it can easily cluster with other Payara Micro
+Servers on the same host. The example uses the autobind feature to determine a free port to use to listen for HTTP requests.
+
+This example also shows how you can add a cluster listener to the Payara Micro runtime so you can be notified when payara Micro
+Servers are started and exit.
+
+Run this example a number of times using
+```shell
+mvn compile exec:exec
+```
---
+++ Payara-Micro/autobind-cluster/src/main/java/fish/payara/examples/payaramicro/autobind/cluster/ClusteredPayaraMicro.java
@@ -0,0 +1,50 @@
+/*
+
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+ Copyright (c) 2015 C2B2 Consulting Limited. All rights reserved.
+
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.examples.payaramicro.autobind.cluster;
+
+import fish.payara.micro.BootstrapException;
+import fish.payara.micro.PayaraMicro;
+import fish.payara.micro.PayaraMicroRuntime;
+import fish.payara.micro.services.PayaraClusterListener;
+import fish.payara.micro.services.data.InstanceDescriptor;
+
+/**
+ *
+ * @author steve
+ */
+public class ClusteredPayaraMicro implements PayaraClusterListener {
+    
+    public static void main(String[] args) throws BootstrapException {
+        PayaraMicroRuntime runtime = PayaraMicro.getInstance()
+                .setHttpAutoBind(true)
+                .setInstanceName("TestInstance")
+                .bootStrap();
+        runtime.addClusterListener(new ClusteredPayaraMicro());
+    }
+
+    @Override
+    public void memberAdded(InstanceDescriptor id) {
+        System.out.println("Payara Micro Instance " + id.getInstanceName() + " has Appeared " + " on Host and http Port " + id.getHostName() + ":" + id.getHttpPort());
+    }
+
+    @Override
+    public void memberRemoved(InstanceDescriptor id) {
+        System.out.println("Payara Micro Instance " + id.getInstanceName() + "has Disappeared " + " on Host and http Port " + id.getHostName() + ":" + id.getHttpPort());
+    }
+    
+}
