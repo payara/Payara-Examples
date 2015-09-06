b738eb334044dda5d81737a1efed2ae7295fa20f
Steve Millidge <steve@payara.co>
2015-09-06 22:06:32 +0100

    Added REST JCache Example

---
+++ rest-examples/rest-jcache/README.md
@@ -0,0 +1,22 @@
+# rest-jcache
+
+A simple example consisting of a REST service that stores JSON into the cache based on a key
+The example uses the standard JCache annotations to retrieve and store data into a cache
+
+Deploy onto Payara Micro using
+```shell
+java -jar payara-micro.jar --autoBindHttp --deploy rest-jcache-1.0-SNAPSHOT.war
+```
+
+Once deployed on Payara Micro test insert date using;
+
+```shell
+curl -H "Accept: application/json" -H "Content-Type: application/json" -X PUT -d "{data}" http://127.0.0.1:8080/rest-jcache-1.0-SNAPSHOT/webresources/cache?key=test
+```
+
+Retrieve data using
+```shell
+curl http://127.0.0.1:8080/rest-jcache-1.0-SNAPSHOT/webresources/cache?key=test
+```
+
+Also run a number of Payara Micro nodes and retrieve the same key from each and you should receive the same data as the cache is shared across all the Payara Micro instances
---
+++ rest-examples/rest-jcache/src/main/java/fish/payara/examples/rest/rest/jcache/ApplicationConfig.java
@@ -0,0 +1,47 @@
+/*
+
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+ Copyright (c) 2015 C2B2 Consulting Limited. All rights reserved.
+
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.examples.rest.rest.jcache;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author steve
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(fish.payara.examples.rest.rest.jcache.CacheResource.class);
+    }
+    
+}
