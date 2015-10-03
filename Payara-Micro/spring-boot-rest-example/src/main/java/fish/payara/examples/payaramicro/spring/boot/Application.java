cba6b90541a69759db37dcde3935c14aa021e128
Mert ÇALIŞKAN <mert@t2.com.tr>
2015-10-03 15:31:19 +0300

    added spring-boot example for Payara Micro

---
+++ Payara-Micro/spring-boot-rest-example/src/main/java/fish/payara/examples/payaramicro/spring/boot/Application.java
@@ -0,0 +1,22 @@
+package fish.payara.examples.payaramicro.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by mertcaliskan
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
