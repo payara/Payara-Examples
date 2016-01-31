c1d46404cc8a91d441d51b158bbbe93ffd2b8d59
Mike Croft <backtotheoldhouse@gmail.com>
2016-01-31 13:09:37 +0000

    added a basic JMS example with producer and consumer

---
+++ Java-EE/simple-jms-example/src/main/java/fish/payara/examples/jms/MessageConsumer.java
@@ -0,0 +1,53 @@
+/*
+
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+ Copyright (c) 2016 C2B2 Consulting Limited. All rights reserved.
+
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.examples.jms;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ *
+ * @author Mike Croft
+ */
+//@MessageDriven(activationConfig = {
+//    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+//    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/simpleQ")
+//})
+@MessageDriven(mappedName = "jms/simpleQ", activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class MessageConsumer implements MessageListener {
+    
+    public MessageConsumer() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("Message received: " + message.getBody(String.class));
+        } catch (JMSException ex) {
+            Logger.getLogger(MessageConsumer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
