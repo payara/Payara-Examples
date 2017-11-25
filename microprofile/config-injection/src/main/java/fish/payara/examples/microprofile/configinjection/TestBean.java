617ca3af2dadb3a1cd2addce359ca8ace7d7dcd2
Steve Millidge <smillidge@users.noreply.github.com>
2017-11-25 21:46:20 +0000

    Added Request Scoped Bean (#67)

--- microprofile/config-injection/src/main/java/fish/payara/examples/microprofile/configinjection/TestBean.java
+++ microprofile/config-injection/src/main/java/fish/payara/examples/microprofile/configinjection/TestBean.java
@@ -5,7 +5,10 @@
  */
 package fish.payara.examples.microprofile.configinjection;
 
+import java.time.LocalDate;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  *
@@ -13,5 +16,61 @@ import javax.enterprise.context.RequestScoped;
  */
 @RequestScoped
 public class TestBean {
+        
+    @Inject
+    @ConfigProperty(name = "default.property", defaultValue = "Default Value")
+    String defaultProperty;
+    
+    // Example showing reading a config property from the META-INF/microprofile-config.properties file
+    @Inject
+    @ConfigProperty(name = "file.property")
+    String fileProperty;
+    
+    // Example config property that uses a default converter to LocalDate
+    @Inject
+    @ConfigProperty(name = "date.property")
+    LocalDate date;
+
+    // Example injection the standard environment variable home
+    @Inject
+    @ConfigProperty(name = "HOME", defaultValue = "HOME environment variable not set")
+    String home;
+    
+    // Example injection of the standard System Property java.home
+    @Inject
+    @ConfigProperty(name = "java.home", defaultValue = "java.home environment variable not set")
+    String javaHome;
+    
+// Example showing reading a config property from the META-INF/microprofile-config.properties file
+    @Inject
+    @ConfigProperty(name = "application.property")
+    String applicationProperty;
+    
+
+    public String getDefaultProperty() {
+        return defaultProperty;
+    }
+
+    public String getFileProperty() {
+        return fileProperty;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public String getJavaHome() {
+        return javaHome;
+    }
+
+    public String getApplicationProperty() {
+        return applicationProperty;
+    }
+
+
     
 }
