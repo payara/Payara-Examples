9810ac125aa7d5c217f1bc1dd98dfc8978ecae0f
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-12-13 19:27:27 -0500

    Modified UI to display a success message upon successful fund transfer.

--- Payara-Micro/payara-micro-event-sourcing-example/fundtransferui/src/main/java/fish/payara/eventsourcing/fundtransferui/controller/FundTransferController.java
+++ Payara-Micro/payara-micro-event-sourcing-example/fundtransferui/src/main/java/fish/payara/eventsourcing/fundtransferui/controller/FundTransferController.java
@@ -7,8 +7,10 @@ import fish.payara.eventsourcing.fundtransferui.webserviceclient.CheckingAcctSer
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.faces.event.ActionEvent;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -19,24 +21,34 @@ import javax.inject.Named;
 @ViewScoped
 public class FundTransferController implements Serializable {
 
+    private FacesContext facesContext = FacesContext.getCurrentInstance();
+
     private static final Logger LOGGER = Logger.getLogger(FundTransferController.class.getName());
 
-    public void transferFunds(ActionEvent actionEvent) {
+    public void transferFunds() {
         LOGGER.log(Level.INFO, String.format("{0}.transferFunds() invoked", this.getClass().getClass()));
         FundTransferDTO fundTransferDTO = new FundTransferDTO();
-        CheckingAcctServiceClient checkingAcctServiceClient = new CheckingAcctServiceClient();
+        CheckingAcctServiceClient checkingAcctServiceClient;
         String fundTransferDTOJson;
 
         fundTransferDTO.setSourceAcctNbr(1123L);
         fundTransferDTO.setDestAcctNbr(1234L);
         fundTransferDTO.setTransactionType(TransactionType.WITHDRAWAL);
         fundTransferDTO.setAmt(100.00);
-
-        fundTransferDTOJson = FundTransferDTOUtil.fundTransferDTOToJson(fundTransferDTO);
-
-        checkingAcctServiceClient.transferFunds(fundTransferDTOJson);
-
-        checkingAcctServiceClient.close();
+        try {
+            checkingAcctServiceClient = new CheckingAcctServiceClient();
+            fundTransferDTOJson = FundTransferDTOUtil.fundTransferDTOToJson(fundTransferDTO);
+            checkingAcctServiceClient.transferFunds(fundTransferDTOJson);
+            checkingAcctServiceClient.close();
+            facesContext.addMessage("msgs", new FacesMessage(FacesMessage.SEVERITY_INFO, 
+                    "Transfer successful", 
+                    "Transfer successful"));
+        } catch (Exception e) {
+            facesContext.addMessage("msgs", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "There was an error transferring funds",
+                    "There was an error transferring funds"));
+            e.printStackTrace();
+        }
     }
 
 }
