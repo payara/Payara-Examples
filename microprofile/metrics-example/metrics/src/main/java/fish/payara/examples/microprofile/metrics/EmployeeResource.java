2ffbc8608b9cdb39cd19dafe0d762f040a42c8c6
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2018-03-09 19:37:07 +0100

    Renamed the project to metrics-example Netbeans was deploying by default as /metrics, colliding with the standard metrics REST endpoint Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

---
+++ microprofile/metrics-example/metrics/src/main/java/fish/payara/examples/microprofile/metrics/EmployeeResource.java
@@ -0,0 +1,78 @@
+package fish.payara.examples.microprofile.metrics;
+
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Metric;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import java.util.ArrayList;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+
+/**
+ * @author Mike Croft
+ */
+
+@Path("employees")
+@ApplicationScoped
+public class EmployeeResource {
+
+    private ArrayList<String> employees = new ArrayList<String>() {{
+        add("John");
+        add("Paul");
+        add("George");
+        add("Ringo");
+    }};
+
+    @Inject
+    @Metric
+    Counter employeeCount;
+
+    @PostConstruct
+    private void init() {
+        // initialise counter with beginning number
+        employeeCount.inc(4);
+    }
+
+    @GET
+    @Path("{id}")
+    public String getEmployeeById(@PathParam("id") int id) {
+        return employees.get(id);
+    }
+
+    @GET
+    public String getAllEmployees() throws InterruptedException {
+        return employees.toString();
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Gauge(name = "numberOfEmployees", unit = MetricUnits.NONE)
+    public int getNumberOfEmployees() {
+        return employees.size();
+    }
+
+    @POST
+    @Metered
+    public void createEmployee(@FormParam("name") String emp) {
+        if (employees.add(emp)) employeeCount.inc();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Metered
+    public void deleteEmployee(@PathParam("id") int id) {
+        if (id >= employees.size()) {
+            return;
+        } else {
+            employees.remove(id);
+            employeeCount.dec();
+        }
+    }
+
+}
