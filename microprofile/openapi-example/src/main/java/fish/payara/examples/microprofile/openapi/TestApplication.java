a66189cd3dd31ae5e55819cb94c0b74ef06ba127
luiseufrasio <luis.eufrasio@gmail.com>
2022-08-01 08:38:38 -0300

    FISH-6298 adapting to jakarta 10

--- microprofile/openapi-example/src/main/java/fish/payara/examples/microprofile/openapi/TestApplication.java
+++ microprofile/openapi-example/src/main/java/fish/payara/examples/microprofile/openapi/TestApplication.java
@@ -1,7 +1,7 @@
 package fish.payara.examples.microprofile.openapi;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class TestApplication extends Application {
